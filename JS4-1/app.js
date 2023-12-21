04ada1d07d143fef9b580470404f6adc70dc12c1
BibeM <150192623+BibeM@users.noreply.github.com>
2023-12-21 20:42:58 +0100

    Add files via upload

---
+++ JS4-1/app.js
@@ -0,0 +1,22 @@
+console.log("i am not sleeping!");
+
+/*const buttonName = document.querySelector("#nameBtn");
+console.log(buttonName);
+
+buttonName.addEventListener("click", function () {
+  console.log("Hello Biljana!");
+}); */
+
+const firstName = document.querySelector("#firstname");
+const lastName = document.querySelector("#lastname");
+const age = document.querySelector("#age");
+const submitBtn = document.querySelector("#submit");
+
+const student = {
+  firstName: firstName.value,
+  lastName: lastName.value,
+  age: age.value,
+};
+function userInfo(firstName, lastName, age) {
+  let studentInfo = [];
+}
